a21ed58da24d50ec121087e2163d55f526002f03
Umoh Mildred <umohmila47@gmail.com>
2026-08-31 19:12:38 +0100

    fix: [Performance] Donation transaction confirmation modal re-ren (#1147)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,1 +1,6 @@
-export { default, type ModalProps } from "./ui/Modal";
+import { memo } from "react";
+import Modal from "./ui/Modal";
+
+const MemoizedModal = memo(Modal);
+
+export default MemoizedModal;(export t9pe { ModalProps } from "./ui/Modal";
